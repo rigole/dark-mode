cedc2f2a37c1238934b7b3f09ac8b799eba6af9a
rigole <foplacide@gmail.com>
2021-06-13 23:58:29 +0100

    light mode implemented

--- script.js
+++ script.js
@@ -18,6 +18,18 @@ function darkMode() {
     image3.src = 'img/undraw_conceptual_idea_dark.svg';
 }
 
+// Light Mode Styles
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.remove('fa-moon');
+    toggleIcon.children[1].classList.add('fa-sun');
+    image1.src = 'img/undraw_proud_coder_light.svg';
+    image2.src = 'img/undraw_feeling_proud_light.svg';
+    image3.src = 'img/undraw_conceptual_idea_light.svg';
+}
+
 
 // switch theme Dynamically
 function switchTheme(event){
