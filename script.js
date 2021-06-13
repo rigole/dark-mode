edbfdc49ba7cbe1af01dca906379eced0430eea7
rigole <foplacide@gmail.com>
2021-06-13 07:20:24 +0100

    dark mode implemented

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
 // switch theme Dynamically
 function switchTheme(event){
-    console.log(event.target.checked);
+    if (event.target.checked){
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else{
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
 }
 // Event Listener
 toggleSwitch.addEventListener('change',switchTheme);
